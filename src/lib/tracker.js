32ca470c3212bcd69bf08d5f2242641097e6d6e9
yanivkalfa <yanivkalfa@yanivkalfas-Mac.local>
2016-03-08 18:30:21 -0500

    fixed export and remove unneeded copy rights

--- src/lib/tracker.js
+++ src/lib/tracker.js
@@ -1,22 +1,8 @@
-/*
- * Node.js tracker for Snowplow: tracker.js
- * 
- * Copyright (c) 2014-2015 Snowplow Analytics Ltd. All rights reserved.
- *
- * This program is licensed to you under the Apache License Version 2.0,
- * and you may not use this file except in compliance with the Apache License Version 2.0.
- * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the Apache License Version 2.0 is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
- */
-
+"use strict";
 import core from 'snowplow-tracker-core';
 
 /**
- * Snowplow Node.js Tracker
+ * Snowplow Tracker
  *
  * @param { string } emitters or array emitters The emitter or emitters to which events will be sent
  * @param { string } namespace The namespace of the tracker
