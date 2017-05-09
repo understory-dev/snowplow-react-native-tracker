520b999b0269f4b3b180f904539744fd8a496a66
Yaniv kalfa <netameta1@yahoo.com>
2017-05-09 15:48:32 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 /*
  * Exporting 3 main components
  */
-import Tracker from './lib/tracker.js';
-import Emitter from './lib/emitter.js';
-import Version from './lib/version.js';
+import tracker from './lib/tracker.js';
+import emitter from './lib/emitter.js';
+import version from './lib/version.js';
 
 export default{ Tracker, Emitter, Version}
